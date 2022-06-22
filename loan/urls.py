f35348e2aa3eb3cb1c27a1537eef402812c4185d
brianmwevi <mwevibrian@gmail.com>
2022-06-23 01:20:06 +0300

    ft:loan post url -- add url for posting loans

--- loan/urls.py
+++ loan/urls.py
@@ -1,17 +1,18 @@
-from django.urls import path,re_path
+from django.urls import path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
 from django.urls import path
 
-urlpatterns =[
+urlpatterns = [
     path('', views.home, name="home"),
     path('about', views.about, name="about"),
     path('faqs', views.faqs, name="faqs"),
     path('profile/<int:pk>/', views.profile, name="profile"),
-    path('apply_loan/<user_id>', views.apply_loan, name="apply_loan"),   
-    
+    path('apply_loan/<user_id>/', views.apply_loan, name="apply_loan"),
+    path('approve_loan/<borrower_id>/', views.approve_loan, name="approve_loan"),
+
     path('lender', views.lenderpage, name="lenderpage"),
-    
-    
-]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
